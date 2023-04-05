e7d2edb4a38b03379740e04cd819418f5d4bcaa6
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-04-05 15:38:47 +0300

    Ülesanne 1 tehtud

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
     const expenses = [
         {
-            date : new Date(2023,0,10),
-            title : 'New book',
-            price : 30.99
+            date: new Date(2023, 0, 10),
+            title:'New book',
+            price: 30.99
         },
         {
-            date : new Date(2023,0,10),
-            title : 'New jeans',
-            price : 99.99
+            date: new Date(2023, 0, 5),
+            title:'New jeans',
+            price: 3.99
         }
-        ];
+    ]
 
-  return (
-    <div className="App">
-        <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
-        <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+            <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
+        </div>
+    );
 }
+
 
 export default App;
