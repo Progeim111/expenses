3e3c7be93ccde5a8a03b8901de5acf4564c1d4bc
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-04-19 12:39:46 +0300

    14. Ülesanne 5 - valikulise kuvamise täiendamine

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('')
-   const [enteredAmount, setEnteredAmount] = useState('')
-   const [enteredDate, setEnteredDate] = useState('')
-
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+    const [isEditing, setIsEditing] = useState(false)
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value)
@@ -26,28 +26,48 @@ const ExpenseForm = (props) => {
         setEnteredTitle('')
         setEnteredAmount('')
         setEnteredDate('')
+        setIsEditing(false)
+    }
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
+    let expenseFormContent = (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+    )
+
+    if (isEditing) {
+        expenseFormContent = (
+            <form onSubmit={submitHandler}>
+                <div className={"new-expense__controls"}>
+                    <div className={"new-expense__control"}>
+                        <label>Title</label>
+                        <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
+                    </div>
+                    <div className={"new-expense__control"}>
+                        <label>Amount</label>
+                        <input type="number" min="1" step="1" onChange={amountChangeHandler}/>
+                    </div>
+                    <div className={"new-expense__control"}>
+                        <label>Date</label>
+                        <input type="date" min="2019-01-01" max="2025-12-31" onChange={dateChangeHandler}/>
+                    </div>
+                    <div>
+                        <button type="button" onClick={stopEditingHandler}>Cancel</button>
+                        <button type="submit">Add Expense</button>
+                    </div>
+                </div>
+            </form>
+        )
     }
 
     return (
-        <form onSubmit={submitHandler}>
-            <div className={"new-expense__controls"}>
-                <div className={"new-expense__control"}>
-                <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
-            </div>
-            <div className={"new-expense__control"}>
-                <label>Amount</label>
-                <input type="number" min="1" step="1" onChange={amountChangeHandler}/>
-            </div>
-            <div className={"new-expense__control"}>
-                <label>Date</label>
-                <input type="date" min="2019-01-01" max="2025-12-31" onChange={dateChangeHandler}/>
-            </div>
-            <div>
-                <button type="submit">Add Expense</button>
-            </div>
-            </div>
-        </form>
+        <div className="new-expense">
+            {expenseFormContent}
+        </div>
     )
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
-import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react';
+import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
-
 
 const NewExpense = (props) => {
     const [lastId, setLastId] = useState(3);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
-            id: 'e' + (lastId + 1)
-        }
-        setLastId(lastId + 1)
-        props.onAddExpense(expenseData)
-    }
-    return (
-        <div className={'new-expense'}>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
-        </div>
-    )
-}
+            id: 'e' + (lastId + 1),
+        };
+        setLastId(lastId + 1);
+        props.onAddExpense(expenseData);
+    };
 
-export default NewExpense
+    return (
+        <div className="new-expense__control">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
+};
+
+export default NewExpense;
